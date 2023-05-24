44009da4e7a03f6a01c0b1f62914295d8045455f
slorens <js.slorens@gmail.com>
2023-05-25 00:24:31 +0300

    Add project files.

---
+++ UserInputProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class UserInputProvider
+    {
+        public int[] getUserInput(int amount)
+        {
+            List<int> numbers = new List<int>();
+            string[] suffixes = { "st", "nd", "rd" };
+
+            for (int i = 0; i < amount; i++)
+            {
+                int temp;
+
+                do
+                {
+                    Console.WriteLine($"Please enter the {i + 1}{suffixes[i]} number");
+                } while (!int.TryParse(Console.ReadLine(), out temp));
+                numbers.Add(temp);
+
+            }
+            return numbers.ToArray();
+        }
+    }
+}
