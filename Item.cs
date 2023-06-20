6ded54e5d6a0b392b422f11da97b0e661446837f
slorens <js.slorens@gmail.com>
2023-06-20 03:06:20 +0300

    Implementing IComparable interface, adding required logic

---
+++ Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Item: IComparable
+    {
+        public string Name { get; set; }
+
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Item other = obj as Item;
+
+            if (other == null)
+            {
+                throw new ArgumentException("Please, provide an Item object");
+            }
+
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            
+            UserInputProvider provider = new UserInputProvider();
+            // Cities by length, then by name
+            List<string> cities = new List<string> { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            List<Item> cityItems = cities.Select((x) =>  new Item(x))
+                .OrderBy(x => x.Name.Length)
+                .ThenBy(x => x.Name)
+                .ToList();
+
+            foreach (Item cityItem in cityItems)
+            {
+                Console.WriteLine(cityItem.Name);
+            }
+
+            Console.WriteLine("\n");
+
+            // Food, distinct elements in ascending order
+            List<string> food = new List<string> { "Butter", "Honey", "Bread", "Biscuit", "Honey" };
+            List<Item> foodItems = food.Select((x) => new Item(x))
+                .DistinctBy(x => x.Name)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            foreach (Item foodItem in foodItems)
+            {
+                Console.WriteLine(foodItem.Name);
+            }
+
+            Console.WriteLine("\n");
+
+            // Input strings and min length, then find the strings of a specified min length
+            const int amount = 3;
+            List<string> strings = new List<string>();
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine($"Please enter a string #{i + 1}:");
+                var input = Console.ReadLine();
+                if (input != null)
+                {
+                    strings.Add(input);
+                }
+
+            }
+
+            int minLength = provider.getUserInput(1)[0];
+
+            List<Item> stringItems = strings.Select(x => new Item(x))
+                .Where(x => x.Name.Length >= minLength)
+                .ToList();
+
+            foreach (Item stringItem in stringItems)
+            {
+                Console.WriteLine(stringItem.Name);
+            }
+
         }
     }
 }
